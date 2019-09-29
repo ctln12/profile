696a81d7af5f606c44644afe313fd935d908d337
Cataline Baert <snuf_kin@yahoo.com>
2019-09-29 11:29:05 +0200

    move javascript to separate file and add link in html

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("HELLO");
+// const sections = document.querySelectorAll(".sections");
+// console.log(sections.forEach(
+//   (item) => {
+//     item.addEventListener('click', (event) => {
+//       event.currentTarget.classList.add("active");
+//     });
+// }));
